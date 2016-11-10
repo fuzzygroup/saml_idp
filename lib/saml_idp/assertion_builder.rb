c0bcb11581d33ced067ae0a900014f9e31b63fac
Scott Johnson <fuzzygroup@gmail.com>
2016-11-10 12:39:53 -0500

    Added saml tags

--- lib/saml_idp/assertion_builder.rb
+++ lib/saml_idp/assertion_builder.rb
@@ -69,9 +69,11 @@ module SamlIdp
                 subject.NameID name_id, Format: name_id_format[:name], xmlns: "urn:oasis:names:tc:SAML:2.0:assertion"
                 
                 subject.SubjectConfirmation Method: Saml::XML::Namespaces::Methods::BEARER do |confirmation|
-                  confirmation.SubjectConfirmationData "", InResponseTo: saml_request_id,
-                    NotOnOrAfter: not_on_or_after_subject,
-                    Recipient: saml_acs_url
+                  assertion.tag!("saml:SubjectConfirmationData", {}) do |sc|
+                    confirmation.SubjectConfirmationData "", InResponseTo: saml_request_id,
+                      NotOnOrAfter: not_on_or_after_subject,
+                      Recipient: saml_acs_url
+                  end
                 end
               end
             end
