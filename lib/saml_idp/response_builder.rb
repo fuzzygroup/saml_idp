16231cfc9272f3dd0d1b8b33b98ac861754277ad
Scott Johnson <fuzzygroup@gmail.com>
2016-11-10 13:33:53 -0500

    brought back the mindtouch type saml which worked -- responses

--- lib/saml_idp/response_builder.rb
+++ lib/saml_idp/response_builder.rb
@@ -28,6 +28,29 @@ module SamlIdp
     end
     private :encode
 
+    # def build
+    #   builder = Builder::XmlMarkup.new
+    #   builder.tag! "samlp:Response",
+    #     ID: response_id_string,
+    #     Version: "2.0",
+    #     IssueInstant: now_iso,
+    #     Destination: saml_acs_url,
+    #     Consent: Saml::XML::Namespaces::Consents::UNSPECIFIED,
+    #     InResponseTo: saml_request_id,
+    #     "xmlns:samlp" => Saml::XML::Namespaces::PROTOCOL do |response|
+    #       #response.Issuer issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
+    #       response.tag! "samlp:Issuer", issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
+    #         #<samlp:Issuer xmlns="urn:oasis:names:tc:SAML:2.0:assertion">http://sso.interania.com/saml/auth</samlp:Issuer>
+    #         #issuer.tag! "samlp:"
+    #         #end
+    #       response.tag! "samlp:Status" do |status|
+    #         status.tag! "samlp:StatusCode", Value: Saml::XML::Namespaces::Statuses::SUCCESS
+    #       end
+    #       response << assertion_and_signature
+    #     end
+    # end
+    # private :build
+    
     def build
       builder = Builder::XmlMarkup.new
       builder.tag! "samlp:Response",
@@ -38,11 +61,7 @@ module SamlIdp
         Consent: Saml::XML::Namespaces::Consents::UNSPECIFIED,
         InResponseTo: saml_request_id,
         "xmlns:samlp" => Saml::XML::Namespaces::PROTOCOL do |response|
-          #response.Issuer issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
-          response.tag! "samlp:Issuer", issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
-            #<samlp:Issuer xmlns="urn:oasis:names:tc:SAML:2.0:assertion">http://sso.interania.com/saml/auth</samlp:Issuer>
-            #issuer.tag! "samlp:"
-            #end
+          response.Issuer issuer_uri, xmlns: Saml::XML::Namespaces::ASSERTION
           response.tag! "samlp:Status" do |status|
             status.tag! "samlp:StatusCode", Value: Saml::XML::Namespaces::Statuses::SUCCESS
           end
